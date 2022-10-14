61ea19cb821ac0c01cc191255b35a5fc8ba5e808
Ailton Filho <65234307+Ailton-F@users.noreply.github.com>
2022-10-14 08:26:27 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 $('.aval').click(function(){
-    alert('oi');
+    $.ajax({
+        url: '../../aval-buttons.html',
+    }).done(function(res){
+        $('.btn-container').empty();
+        $('.btn-container').append(res);
+        
+    }).fail(function(JHXRql, textStatus){
+        console.log(JHXRql+ ' ' +textStatus);
+    });
 });
