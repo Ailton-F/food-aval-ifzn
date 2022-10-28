1fa013bddf670a1b642fc9c781e49950d43ab95b
Ailton Borges de Lima Filho <ailtonfilho@NATALCENTER.LOCAL>
2022-10-28 14:10:54 -0300

    finished funcionalities of first MVP

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,60 +8,65 @@ var chart = new CanvasJS.Chart("graphic", {
         interval: 1
     },
     data: [{
-        type: "bar",
+        type: "column",
         name: "companies",
         axisYType: "secondary",
         color: "#014D65",
         dataPoints: [
-            { y: 3, label: "Ótimo" },
-            { y: 7, label: "Bom" },
-            { y: 5, label: "Ruim" }
+            { y: 0, label: "Ótimo", color: '#4A993D'},
+            { y: 0, label: "Bom", color: '#E9EE00'},
+            { y: 0, label: "Ruim", color: '#FF5B5B'}
         ]
     }]
 });
+
+
 chart.render();
 
-$('.aval').click(function(){
+function upVal(dataPointPos, text){
+    Swal.fire({
+        icon: 'success',
+        title: `Certeza que quer votar em "${text}"?`,
+        confirmButtonText: 'Sim',
+        showCancelButton: true,
+    }).then(function(res){
+        if(res.isConfirmed){
+            chart.options.data[0].dataPoints[dataPointPos].y += 1
+            chart.render()
+            $.ajax({
+                url: '../../index.html',
+                datatype: 'html'
+            }).done(function(res){
+                $('.btn-container').empty();
+                $('.btn-container').append($(res).find('.aval')); 
+                $('.aval').click(aval);
+            }).fail(function(){
+
+            })
+        }
+    });
+}
+function aval(){
     $.ajax({
         url: '../../aval-buttons.html',
     }).done(function(res){
         $('.btn-container').empty();
         $('.btn-container').append(res);
         
-        
-        // let value_c1 = parseInt($('.contador-1>span').text());
-        // let value_c2 = parseInt($('.contador-2>span').text());
-        // let value_c3 = parseInt($('.contador-3>span').text());
-
-        // function upVal(text, valToUp, divToReload){
-        //     Swal.fire({
-        //         icon: 'success',
-        //         title: `Certeza que quer votar em "${text}"?`,
-        //         confirmButtonText: 'Sim',
-        //         showCancelButton: true,
-        //     }).then(function(res){
-        //         if(res.isConfirmed){
-        //             valToUp += 1;
-        //             divToReload.text(value_c1);
-        //         }
-        //     });
-        // }
-
-        // $('.aval-good').click(function(){
-        //     upVal('Ótimo', value_c1, $('.contador-1>span'));
-        // });
-
-        // $('.aval-so').click(function(){
-        //     upVal('Ok', value_c2, $('.contador-2>span'));
-        // });
-
-        // $('.aval-bad').click(function(){
-        //     upVal('Péssimo', value_c3, $('.contador-3>span'));
-        // });
-        
+        $('.aval-good').click(()=>{
+            upVal(0, $('.aval-good').text())
+        });
+    
+        $('.aval-so').click(()=>{
+            upVal(1, $('.aval-so').text())
+        });
+    
+        $('.aval-bad').click(()=>{
+            upVal(2, $('.aval-bad').text())
+        });
     }).fail(function(JHXRql, textStatus){
         console.log(JHXRql+ ' ' +textStatus);
     });
-    
-    
-});
+}
+
+$('.aval').click(aval);
